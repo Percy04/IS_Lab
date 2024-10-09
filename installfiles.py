cda9a2b8334f96c51614bdb222511d11e9f9b316
Saptashwa Mandal <44750476+Percy04@users.noreply.github.com>
2024-10-09 07:32:26 +0530

    Add files via upload

---
+++ installfiles.py
@@ -0,0 +1,7 @@
+# 1. Download the source code
+# 2. unzip pycryptodome.zip   # If it's a .zip file
+# tar -xzvf pycryptodome.tar.gz   # If it's a .tar.gz file
+
+# 3. cd pycryptodome
+# 4. python setup.py install
+# 5. python -c "import Crypto; print(Crypto.__version__)"
